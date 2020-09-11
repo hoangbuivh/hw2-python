f2969b0c72446d11e81be7e9fe0e1ea7bdb609e1
Hoang  Bui <hhb5051@psu.edu>
2020-09-11 15:39:07 +0000

    getGradePoint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Author Hoang Bui hhb5051@psu.edu
-def getGradeScore(grade): 
+def getGradePoint(grade): 
   if grade == "A": 
     return 4.0
   elif grade == "A-": 
@@ -22,17 +22,17 @@ def getGradeScore(grade):
 def run():
   letterGrade1 = input("Enter your course 1 letter grade: ")
   credit1 = float(input("Enter your course 1 credit: "))
-  print(f"Grade point for course 1 is: {getGradeScore(letterGrade1)}")
+  print(f"Grade point for course 1 is: {getGradePoint(letterGrade1)}")
 
   letterGrade2 = input("Enter your course 2 letter grade: ")
   credit2 = float(input("Enter your course 2 credit: "))
-  print(f"Grade point for course 2 is: {getGradeScore(letterGrade2)}")
+  print(f"Grade point for course 2 is: {getGradePoint(letterGrade2)}")
 
   letterGrade3 = input("Enter your course 3 letter grade: ")
   credit3 = float(input("Enter your course 3 credit: "))
-  print(f"Grade point for course 3 is: {getGradeScore(letterGrade3)}")
+  print(f"Grade point for course 3 is: {getGradePoint(letterGrade3)}")
 
-  gpa = (getGradeScore(letterGrade1)*credit1 + getGradeScore(letterGrade2)*credit2 + getGradeScore(letterGrade3)*credit3)/(credit1+credit2+credit3)
+  gpa = (getGradePoint(letterGrade1)*credit1 + getGradePoint(letterGrade2)*credit2 + getGradePoint(letterGrade3)*credit3)/(credit1+credit2+credit3)
   print(f"Your GPA is: {gpa}")
   
 if __name__ == "__main__": 
